f69d845b3a6bfba98401cb0b4a5ecde725925923
ALI Azhar <devileye1133@gmail.com>
2026-07-16 19:10:46 +0500

    03_nversion

--- d.js
+++ d.js
@@ -14,4 +14,31 @@ accountCity = "gujranwala"
 console.log(accountId);
 
 
-console.table(accountEmail, accountId, accountPassword, accountCity)
+console.table({ accountEmail, accountPassword, accountCity });
+ // data type //
+
+//"use strict"; // treat all js  code as newer version 
+
+// alert (3+3) // we are using nodejs, not brower
+
+
+ let name = "ali"
+ let age = 23
+ let isloggedin = false 
+  
+
+ // number => 2 to power 53
+ // bigint 
+ // string =>""
+ //boolean => ture/false
+ //null => standalone value 
+  
+//undefined =>
+    // symbole => unique
+
+
+// object  
+
+console.log (typeof age )
+console .log (typeof null)
+console.log (typeof undefined)
