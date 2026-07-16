ea686b1702f87652da7403c4faabbb008a4fd491
ALI Azhar <devileye1133@gmail.com>
2026-07-16 14:47:05 +0500

    basic.01

--- d.js
+++ d.js
@@ -1,1 +1,17 @@
-hello
+console.log("hello")
+const accountId = 14455352
+let accountEmail = "devileye1133@gmal.com"
+var accountPassword = "123455"
+accountCity = "kamoke"
+ 
+
+accountEmail = " hello@.com"
+accountPassword = "35363636"
+accountCity = "gujranwala"
+
+
+
+console.log(accountId);
+
+
+console.table(accountEmail, accountId, accountPassword, accountCity)
