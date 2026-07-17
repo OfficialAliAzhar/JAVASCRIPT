9d128f5e09b43e0c57a8f2081f80bbdfba8cbcc2
ALI Azhar <devileye1133@gmail.com>
2026-07-17 16:46:14 +0500

    data_type.js

---
+++ data_type.js
@@ -0,0 +1,37 @@
+//  primitive
+
+// 7 type :  string, number, boolearn, null, undefined, symbol,
+// BigInt
+
+const socre = 100
+const socreValue = 100.3
+
+ const isloggedin =  false
+ const outsideTemp = null
+let userEmail;
+
+const id = Symbol ('234')
+ const anotherid = Symbol ('234')
+ 
+
+console.log(id === anotherid);
+
+const bignumber = 312654846135468468435981435498741n
+
+
+
+
+// reference ( non  primitive)
+// array, objective, funtion
+const heros = ["batman", "supermen", "doge"]
+let myobj ={
+        name: "ali",
+        age: 22,
+}
+
+const myfunction =  function(){
+    console.log("hello woeld");
+
+} 
+
+console.log( typeof bignumber);
